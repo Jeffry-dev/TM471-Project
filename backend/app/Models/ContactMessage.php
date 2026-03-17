ba75595951c95dc253e8e53a5c83897b41ae2d4a
unknown <jhadmin@JEFFBEZOS.LOCAL>
2026-03-17 22:03:44 +0200

    create contact message model and update user model

---
+++ backend/app/Models/ContactMessage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContactMessage extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'subject',
+        'phone',
+        'message',
+        'mode',
+        'date',
+        'occasion',
+        'seating',
+        'preferred_contact',
+    ];
+}
